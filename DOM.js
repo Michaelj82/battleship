a8651e112465e85d8cee9bcea6df59d764513fda
Michael Johnson <mjj.yoyogi@gmail.com>
2022-12-17 16:51:33 -0500

    Made it so you can add ships, now I just need to make it restricted to a certain amount and so that you can change if you misplace

--- DOM.js
+++ DOM.js
@@ -1,6 +1,19 @@
 import{Ship} from './script.js'
+import { Gameboard } from './script.js'
+import { Player } from './script.js'
+
+var Player1Board = Gameboard()
+var Player2Board = Gameboard()
+
+var Player1 = Player(NaN, Player1Board)
+var Player2 = Player(NaN, Player2Board)
+Player1.state.enemy = Player2;
+Player2.state.enemy = Player1;
 
 
+var SELECTION = false
+var SELECTIONNUMBER = 0
+var SELECTIONTYPE = NaN
 
 let site = document.getElementById('site')
 
@@ -27,21 +40,71 @@ function mainPage(parent){
 
 mainPage(site)
 
+function refreshBoardItems(board, gameboard){
+    console.log('yo')
+    let children = board.children;
+    let num = 0
+    for (let i =0 ; i < gameboard.state.board.length; i++){
+        for (let j = 0 ; j < gameboard.state.board[i].length; j++){
+            console.log(gameboard.state.board[i][j])
+            if (gameboard.state.board[i][j] == 1){
+                console.log(children)
+                children[num].classList.add('ship')
+            }
 
-function makeBoard(parent, sizeOfBoard){
+
+            num ++
+        }
+    }
+}
+
+
+function makeBoard(parent, gameboard){
     let board = document.createElement('div')
     board.setAttribute('id', 'board')
     parent.append(board)
+    let total = 0
+
+    for (let i = 0; i < (gameboard.state.board.length); i++){
+        for(let j = 0 ; j < gameboard.state.board[i].length; j++){
+            total ++
+            let tile = document.createElement('div')
+            tile.classList.add('tile')
+            tile.setAttribute('id', `tile${total}`)
+            tile.onclick = function(){
+                if (SELECTION == true){
+                    SELECTION = false
+                    let ship = Ship(SELECTIONNUMBER)
+                    gameboard.placeShip(ship, i, j, SELECTIONTYPE)
+                    console.log(gameboard.state.board)
+                    refreshBoardItems(board, gameboard)
+                }
+            }
+
+            board.appendChild(tile)
+        
+            // if (SELECTION == true){
 
 
-    for (let i = 0; i < (sizeOfBoard * sizeOfBoard); i++){
-        let tile = document.createElement('div')
-        tile.classList.add('tile')
-        tile.setAttribute('id', `tile${i}`)
-        board.appendChild(tile)
+
+
+                // for (let j = 0 ; j < SELECTIONNUMBER; j++){
+                //     if (SELECTIONTYPE == 'Horizontal'){
+                //     let selectTile = document.getElementById(`tile${i+j}`)
+                //     selectTile.classList.add('ship')
+                //     }else if (SELECTIONTYPE == 'Vertical'){
+                //         let selectTile = document.getElementById(`tile${i+(j*7)}`)
+                //         selectTile.classList.add('ship')
+                //     }
+                // }
+
+
+                // SELECTION = false
+            }
+
+        }
     }
 
-}
 
 
 function playerOneSetUp(parent){
@@ -53,7 +116,43 @@ function playerOneSetUp(parent){
     header2.textContent = 'Player 1, put down your ships. Player 2, look away'
     parent.appendChild(header2)
 
-    makeBoard(parent, 7)
+    let allSelections = document.createElement('div');
+    allSelections.setAttribute('id', 'allSelections')
+
+    let numbers = ['One', 'Two', 'Three', 'Four'];
+    for (let i = 0 ; i < 4 ; i++){
+        let ShipSelect = document.createElement('div');
+        ShipSelect.classList.add('shipSelection')
+        ShipSelect.textContent = numbers[i]
+    
+    
+        let Horizontal = document.createElement('button')
+        Horizontal.textContent = 'Horizontal'
+        Horizontal.onclick = function(){
+            SELECTION = true
+            SELECTIONNUMBER = (i + 1)
+            SELECTIONTYPE = 'horizontal'
+        }
+    
+        let Vertical = document.createElement('button')
+        Vertical.textContent = 'Vertical'
+        Vertical.onclick = function(){
+            SELECTION = true
+            SELECTIONNUMBER = (i + 1)
+            SELECTIONTYPE = 'vertical'
+        }
+    
+
+
+        ShipSelect.appendChild(Horizontal)
+        ShipSelect.appendChild(Vertical)
+    
+        allSelections.appendChild(ShipSelect)
+    
+    }
+    parent.appendChild(allSelections)
+    
+    makeBoard(parent, Player1Board)
 
 }
 
